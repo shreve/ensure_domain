46277f0a446686b8a1c399b5137872f67c7cb5e0
Jacob Evan Shreve <jacob@shreve.ly>
2013-11-24 13:36:58 -0500

    Bumped version to 1.0.0

--- lib/ensure_subdomain/version.rb
+++ lib/ensure_subdomain/version.rb
@@ -1,8 +1,8 @@
-class EnsureSubdomain 
+class EnsureSubdomain
 	module VERSION
-		MAJOR = 0
-		MINOR = 9
-		TINY = 9
+		MAJOR = 1
+		MINOR = 0
+		TINY = 0
 
 		STRING = [MAJOR, MINOR, TINY].join('.')
 	end
