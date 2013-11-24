a3420d746afdea44c3dda300f8cd9cbc107ede2b
Jacob Evan Shreve <jacob@shreve.ly>
2013-11-24 13:18:34 -0500

    Added tests, improved EnsureSubdomain#matches, and removed whitespace

--- lib/ensure_subdomain.rb
+++ lib/ensure_subdomain.rb
@@ -1,14 +1,16 @@
 class EnsureSubdomain
 	attr_accessor :subdomain
-	
+
 	def initialize(subdomain)
 		self.subdomain = subdomain.sub(/\.$/, '')
 	end
 
 	def matches?(request)
-		request.subdomain.match(self.subdomain).nil?
+		# ''.match('www') #=> nil, which is is the opposite of what we want
+		(self.subdomain.empty? && request.subdomain.present?) ||
+			request.subdomain.match(self.subdomain).nil?
 	end
-	
+
 	def to(params, request)
 		url = request.protocol
 		url << "#{self.subdomain}." if self.subdomain.present?
@@ -23,14 +25,14 @@ module ActionDispatch::Routing::Mapper::HttpHelpers
 		ensure_subdomain ''
 	end
 	alias_method :ensure_non_www, :ensure_no_www
-	
+
 	def ensure_www
 		ensure_subdomain 'www'
 	end
 
 	def ensure_subdomain(subdomain, options={})
 		redirector = EnsureSubdomain.new( subdomain )
-		verbs = options[:via] || [:get, :post, :put, :patch, :delete] 
+		verbs = options[:via] || [:get, :post, :put, :patch, :delete]
 		constraints( redirector ) do
 			match '/*path', to: redirect { |params, request| redirector.to params, request }, via: verbs
 		end
